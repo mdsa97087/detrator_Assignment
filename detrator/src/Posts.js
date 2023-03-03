a1c341a0a9485d587b4dfd11f4aad926a3e72755
mdsa97087 <mdsa97087@gmail.com>
2023-03-03 12:58:00 +0530

    Assignment

--- detrator/src/Posts.js
+++ detrator/src/Posts.js
@@ -1,10 +1,9 @@
-import { Box } from "@mui/system";
 import axios from "axios";
+import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
 
 const Posts = () => {
   const [data, setData] = useState([]);
- 
 
   const getData = () => {
     return axios.get("https://dummyjson.com/posts");
@@ -16,47 +15,42 @@ const Posts = () => {
         setData(res.data.posts);
         console.log(res.data.posts);
       })
-      .catch((err) => {
-        console.log(err);
+      .catch((Error) => {
+        console.log(Error);
       });
   }, []);
 
-   return (
-       <Box sx={{
-              display:"flex",
-              flexWrap:"wrap",              
-              alignItems: "center",
-              borderRadius: "12px",
-              boxShadow: 1,
-              fontWeight: "bold",
-              gap:"25px",
-            }}>
-      
-
-      {data.map((elem) => (
-          <Box sx={{
-              border: "1px solid",
-              width:"32%",
-              flexDirection: { xs: "column", md: "row" },
-              alignItems: "center",             
-              overflow: "hidden",
-              borderRadius: "12px",              
-              fontWeight: "bold",
-              height:"500px"
-            }}
-            key={elem.id}
-          >
-            <h3>Title: {elem.title}</h3>
-            <span>Body: {elem.body}</span>
-            <p>Reaction: {elem.reactions}</p>
-            <p>UserId: {elem.userId}</p>
-             {elem.tags.map((tag) => (
-             <p>Tags:{tag}</p>  
-            ))} </Box>
-          
-        ))}
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        width: "90vw",
+        flexWrap: "wrap",
+        justifyContent:'center',
+        gap: "20px",
+        margin: "auto",
+      }}
+    >
+      {data.map((el) => (
+        <Box
+          sx={{
+            border: "1px solid",
+            borderRadius: "10px",
+            padding:"5px",
+            flex:'1 1 480px'
+          }}
+          key={el.id}
+        >
+          <h4>Title: {el.title}</h4>
+          <span>Body: {el.body}</span>
+          <p>Reaction: {el.reactions}</p>
+          <p>UserId: {el.userId}</p>
+          {el.tags.map((tag) => (
+            <p>{tag}</p>
+          ))}
+        </Box>
+      ))}
     </Box>
-   
   );
 };
 
